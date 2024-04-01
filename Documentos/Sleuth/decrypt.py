c8ee1c94e93a63d50d495f8ef6f8be3f987be1f9
hpinedoh <hpinedoh@gmail.com>
2024-04-01 11:48:33 -0600

    new file:   Documentos/Sleuth/decrypt.py 	renamed:    hello-world/hello.py -> Documentos/hello-world/hello.py

---
+++ Documentos/Sleuth/decrypt.py
@@ -0,0 +1,10 @@
+print("Hello, Contosoville!")
+# Este es un comentario que no será interpretado como un comando.
+town = "Contosoville"
+print("The town I am looking for is " + town )
+# DEfiniendo una función power (funtion) to chant a phrase
+def chant(phrase):
+    #pega tres copias e imprime como un mensaje
+    print(phrase + phrase + phrase)
+#invoca la función
+chant("Contosoville! ")
